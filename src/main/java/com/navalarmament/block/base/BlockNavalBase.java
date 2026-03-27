a6ffd51a23a5daa780b27f128fb35a4ea7e56d38
Sierra-117605 <tkmuhei7310@gmail.com>
2026-03-28 01:08:10 +0900

    Phase 6: Add CableNetwork block registration on place/break

--- src/main/java/com/navalarmament/block/base/BlockNavalBase.java
+++ src/main/java/com/navalarmament/block/base/BlockNavalBase.java
@@ -1,8 +1,12 @@
 package com.navalarmament.block.base;
 
 import com.navalarmament.init.NavalCreativeTabs;
+import com.navalarmament.system.CableNetwork;
+import com.navalarmament.tileentity.base.ICableConnectable;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 public abstract class BlockNavalBase extends Block {
 
@@ -12,11 +16,25 @@ public abstract class BlockNavalBase extends Block {
     }
 
     @Override
-    public boolean hasTileEntity(int metadata) {
-        return true;
+    public boolean hasTileEntity(int metadata) { return true; }
+
+    @Override
+    public void onBlockAdded(World world, int x, int y, int z) {
+        if (!world.isRemote) {
+            TileEntity te = world.getTileEntity(x, y, z);
+            if (te instanceof ICableConnectable) {
+                CableNetwork.getInstance().onDeviceConnected(world, x, y, z);
+            }
+        }
     }
 
-    public String getNationId() {
-        return "common";
+    @Override
+    public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
+        if (!world.isRemote) {
+            CableNetwork.getInstance().onCableRemoved(world, x, y, z);
+        }
+        super.breakBlock(world, x, y, z, block, meta);
     }
+
+    public String getNationId() { return "common"; }
 }
--- src/main/java/com/navalarmament/block/common/BlockNavalCable.java
+++ src/main/java/com/navalarmament/block/common/BlockNavalCable.java
@@ -1,9 +1,11 @@
 package com.navalarmament.block.common;
 
 import com.navalarmament.init.NavalCreativeTabs;
+import com.navalarmament.system.CableNetwork;
 import com.navalarmament.tileentity.common.TENavalCable;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
@@ -18,16 +20,28 @@ public class BlockNavalCable extends Block {
     }
 
     @Override
-    public boolean hasTileEntity(int metadata) {
-        return true;
-    }
+    public boolean hasTileEntity(int metadata) { return true; }
 
     @Override
     public TileEntity createTileEntity(World world, int metadata) {
         return new TENavalCable();
     }
 
-    // 隣接ブロック変化時に接続を更新
+    @Override
+    public void onBlockAdded(World world, int x, int y, int z) {
+        if (!world.isRemote) {
+            CableNetwork.getInstance().onCablePlaced(world, x, y, z);
+        }
+    }
+
+    @Override
+    public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
+        if (!world.isRemote) {
+            CableNetwork.getInstance().onCableRemoved(world, x, y, z);
+        }
+        super.breakBlock(world, x, y, z, block, meta);
+    }
+
     @Override
     public void onNeighborBlockChange(World world, int x, int y, int z, Block neighbor) {
         if (world.isRemote) return;
